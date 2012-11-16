6bb7ddd273e59e1696e2b8b5e065a4bf29a979b7
bogdan <bogdan.bugarschi@netapp.com>
2012-11-16 09:27:54 +0200

    Added logging at info level to ALotOfForms class

--- jwic.samples/src/de/jwic/samples/ALotOfForms.java
+++ jwic.samples/src/de/jwic/samples/ALotOfForms.java
@@ -3,6 +3,8 @@ package de.jwic.samples;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import de.jwic.base.ControlContainer;
 import de.jwic.base.IControlContainer;
 import de.jwic.base.JavaScriptSupport;
@@ -12,6 +14,8 @@ import de.jwic.controls.InputBoxControl;
 public class ALotOfForms extends ControlContainer {
 	private static final long serialVersionUID = 1L;
 
+	private static final Logger log = Logger.getLogger(ALotOfForms.class);
+	
 	public ALotOfForms(IControlContainer container) {
 		super(container);
 		init();
@@ -26,7 +30,7 @@ public class ALotOfForms extends ControlContainer {
 		final List<InputBoxControl> boxes = new ArrayList<InputBoxControl>();
 		
 
-		for (int i = 0; i < 20; i++) {
+		for (int i = 0; i < 50; i++) {
 			InputBoxControl inp = new InputBoxControl(this, "box" + i);
 			boxes.add(inp);
 			inp.setText("Some Text " + i);
@@ -34,8 +38,19 @@ public class ALotOfForms extends ControlContainer {
 
 	}
 	
+	private long timestamp;
+	
 	public void actionTest(String param){
-		System.out.println("asd");
 	}	
-
+	
+	
+	public void actionTimestamp_start(String param){
+		timestamp = Long.parseLong(param);
+		log.info("Starting");
+	}
+	public void actionTimestamp_stop(String param){
+		log.info("Finished in: " + (Long.parseLong(param)-timestamp));
+		
+		
+	}
 }
--- jwic.samples/src/de/jwic/samples/ALotOfForms.js
+++ jwic.samples/src/de/jwic/samples/ALotOfForms.js
@@ -6,8 +6,14 @@
 		var button = jQuery('#buttonTest');
 		
 		button.click(function(e){
+			JWic.fireAction(id,'timestamp_start',new Date().getTime());
+			
+			
 			for(var i =0;i<100;i++)
 				JWic.fireAction(id,'test','');
+			
+			
+			JWic.fireAction(id,'timestamp_stop',new Date().getTime());
 		});
 	}
 

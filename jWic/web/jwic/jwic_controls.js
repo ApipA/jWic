6e06a31b5b8042536d68473f3834fc501288a951
bogdan <Bogdan.Bugarschi@netapp.com>
2013-07-12 14:10:42 +0300

    Fixed inputBox placeholder text in IE

--- jWic/web/jwic/jwic_controls.js
+++ jWic/web/jwic/jwic_controls.js
@@ -1636,22 +1636,22 @@ JWic.controls = {
 		 * Initialize a new control.
 		 */
 		initialize : function(inpElm) {
-			inpElm.bind("focus", JWic.controls.InputBoxControl.focusHandler);
-			inpElm.bind("blur", JWic.controls.InputBoxControl.lostFocusHandler);
+			inpElm.bind("focus", function () {
+				var input = jQuery(this);
+				if (input.val() == input.attr('placeholder')) {
+					input.val('');
+					input.removeClass('x-empty');
+				}
+			}).bind("blur", function () {
+				var input = jQuery(this);
+				if (input.val() == '' || input.val() == input.attr('placeholder')) {
+					input.addClass('x-empty');
+					input.val(input.attr('placeholder'));
+				}
+			}).blur();
 			
 			if (inpElm.attr("xListenKeyCode") != 0) {
-				inpElm.bind("keyup", JWic.controls.InputBoxControl.keyHandler);
-			}
-			
-			if (inpElm.attr("xEmptyInfoText")) {
-				if(inpElm.attr("xIsEmpty") == "true" && 
-					(inpElm.val() == inpElm.attr("xEmptyInfoText") || inpElm.val() == "")) {
-					inpElm.addClass("x-empty");
-					inpElm.val(inpElm.attr("xEmptyInfoText"));
-				} else {
-					inpElm.attr("xIsEmpty", "false");
-					inpElm.removeClass("x-empty");
-				}
+				inpElm.bind("keyup", InputBoxControl.keyHandler);
 			}
 			
 			// override the getValue() method to "fix" the serialization
@@ -1665,52 +1665,16 @@ JWic.controls = {
 		 * Clean up..
 		 */
 		destroy : function(inpElm) {
-			inpElm.unbind("focus", JWic.controls.InputBoxControl.focusHandler);
-			inpElm.unbind("blur", JWic.controls.InputBoxControl.lostFocusHandler);
+			inpElm.unbind("focus")
+				.unbind("blur")
+				.unbind("keyup");
 			
-			if (inpElm.attr("xListenKeyCode") != 0) {
-				inpElm.unbind("keyup", JWic.controls.InputBoxControl.keyHandler);
-			}
-		},
-		
-		/**
-		 * Invoked when the focus is received.
-		 */
-		focusHandler : function(e) {
-			var elm =  jQuery(e.target);
-			elm.addClass("x-focus");
-			
-			if (elm.attr("xEmptyInfoText")) {
-				if (elm.attr("xIsEmpty") == "true") {
-					elm.val('');
-					elm.removeClass("x-empty");
-					elm.attr("xIsEmpty", "false");
-				} 
-			}
-			
-		},
-		/**
-		 * Invoked when the focus is lost.
-		 */
-		lostFocusHandler : function(e) {
-			var elm =  jQuery(e.target);
-			
-			elm.removeClass("x-focus");
-			if (elm.attr("xEmptyInfoText")) {
-				if (elm.val() == "") { // still empty
-					elm.addClass("x-empty");
-					elm.val(elm.attr("xEmptyInfoText"));
-					elm.attr("xIsEmpty", "true");
-				} else {
-					elm.attr("xIsEmpty", "false");
-				}
-			}
 		},
 		
 		keyHandler : function(e) {
 			var elm =  jQuery(e.target);
 			
-			if (e.keyCode == elm.attr("xListenKeyCode")) {
+			if (e.keyCode === parseInt(elm.attr("xListenKeyCode"), 10)) {
 				JWic.fireAction(elm.attr('id'), 'keyPressed', '' + e.keyCode);
 			}
 		}
